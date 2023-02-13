74e7499dfa424a67372eb9b6495b78ac398ff437
Alin <alin@gmail.com>
2023-02-13 06:17:58 +0200

    added page routing

---
+++ src/pages/events/[slug].js
@@ -0,0 +1,10 @@
+
+function EventPage() {
+    return (
+        <div>
+            <h1>My Event</h1>
+        </div>
+    )
+}
+
+export default EventPage
---
+++ src/pages/events/add.js
@@ -0,0 +1,10 @@
+
+const Add = () => {
+    return (
+        <div>
+            <h1>Add Event</h1>
+        </div>
+    );
+}
+
+export default Add;
